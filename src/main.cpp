1861f2c252b93775e0898edfaeb424dfc2ded66e
Maksim <kuzminovm78@gmail.com>
2025-08-12 14:20:37 +0500

    REPL

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  // Flush after every std::cout / std:cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+  while (true) {
+    std::cout << "$ ";
+    std::string command;
+    std::getline(std::cin, command);
+    if(command.empty()) {
+      continue;
+    }
+    std::cout << command << ": command not found" << std::endl;
+  }
+}
+
